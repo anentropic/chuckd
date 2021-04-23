9e71640c34a93d71570829e57e2b06f377cd0738
anentropic <ego@anentropic.com>
2021-04-23 11:35:34 +0100

    refactor, add basic picocli cli interface

--- app/src/test/java/chuckd/ChuckDTest.java
+++ app/src/test/java/chuckd/ChuckDTest.java
@@ -1,8 +1,5 @@
 package chuckd;
 
-import io.confluent.kafka.schemaregistry.ParsedSchema;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,17 +7,30 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class AppTest {
+import org.junit.Before;
+import org.junit.Test;
+import picocli.CommandLine;
+
+
+public class ChuckDTest {
     Path resourcesPath = Paths.get("src","test", "resources");
+
+    ChuckD app;
+    CommandLine cmd;
+
+    @Before
+    public void setUp() {
+        app = new ChuckD();
+        cmd = new CommandLine(app);
+    }
 
     @Test public void testAppGetReport() throws IOException {
         String arg0 = resourcesPath.resolve("person-1.0.0.json").toFile().getAbsolutePath();
         String arg1 = resourcesPath.resolve("person-1.1.0.json").toFile().getAbsolutePath();
-
         String[] args = {arg0, arg1};
+        cmd.parseArgs(args);
 
-        List<ParsedSchema> schemas = App.loadSchemas(args);
-        List<String> report = App.getReport(schemas);
+        List<String> report = app.getReport();
 
         assertEquals(report.size(), 1);
     }
